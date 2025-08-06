c64831cc6244ad74fcef5a71f061c151e96812cd
TrienVu27 <nxvtrien1@gmail.com>
2025-08-06 19:15:11 +0700

    add alarm

--- MEVN SWM-G/SWM-G_BasicMotionSample/FormAlarmMonitor.Designer.cs
+++ MEVN SWM-G/SWM-G_BasicMotionSample/FormAlarmMonitor.Designer.cs
@@ -29,6 +29,7 @@ namespace BasicMotionSample
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
@@ -41,16 +42,25 @@ namespace BasicMotionSample
             this.Reset = new System.Windows.Forms.DataGridViewButtonColumn();
             this.AlarmCode = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.check = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.buttonResetAll = new System.Windows.Forms.Button();
+            this.buttonResetChecked = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.buttonClose = new System.Windows.Forms.Button();
             this.groupBox2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox2
             // 
+            this.groupBox2.AutoSize = true;
+            this.groupBox2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.groupBox2.Controls.Add(this.dataGridView1);
             this.groupBox2.Location = new System.Drawing.Point(3, 11);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(341, 286);
+            this.groupBox2.Size = new System.Drawing.Size(364, 301);
             this.groupBox2.TabIndex = 2;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Axes Alarm Monitor";
@@ -86,7 +96,7 @@ namespace BasicMotionSample
             dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
             this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
             this.dataGridView1.GridColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.dataGridView1.Location = new System.Drawing.Point(8, 19);
+            this.dataGridView1.Location = new System.Drawing.Point(18, 22);
             this.dataGridView1.Name = "dataGridView1";
             dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
             dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
@@ -102,7 +112,7 @@ namespace BasicMotionSample
             this.dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle4;
             this.dataGridView1.RowTemplate.DefaultCellStyle.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
             this.dataGridView1.ShowEditingIcon = false;
-            this.dataGridView1.Size = new System.Drawing.Size(327, 260);
+            this.dataGridView1.Size = new System.Drawing.Size(340, 260);
             this.dataGridView1.TabIndex = 2;
             // 
             // Axis
@@ -111,6 +121,8 @@ namespace BasicMotionSample
             this.Axis.MinimumWidth = 6;
             this.Axis.Name = "Axis";
             this.Axis.ReadOnly = true;
+            this.Axis.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Axis.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
             this.Axis.Width = 50;
             // 
             // Status
@@ -119,7 +131,7 @@ namespace BasicMotionSample
             this.Status.MinimumWidth = 6;
             this.Status.Name = "Status";
             this.Status.ReadOnly = true;
-            this.Status.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Status.Resizable = System.Windows.Forms.DataGridViewTriState.False;
             this.Status.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
             this.Status.Width = 60;
             // 
@@ -128,6 +140,7 @@ namespace BasicMotionSample
             this.Reset.HeaderText = "Reset";
             this.Reset.MinimumWidth = 6;
             this.Reset.Name = "Reset";
+            this.Reset.Resizable = System.Windows.Forms.DataGridViewTriState.False;
             this.Reset.Width = 40;
             // 
             // AlarmCode
@@ -135,6 +148,9 @@ namespace BasicMotionSample
             this.AlarmCode.HeaderText = "Alarm Code";
             this.AlarmCode.MinimumWidth = 6;
             this.AlarmCode.Name = "AlarmCode";
+            this.AlarmCode.ReadOnly = true;
+            this.AlarmCode.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.AlarmCode.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
             this.AlarmCode.Width = 150;
             // 
             // check
@@ -144,19 +160,92 @@ namespace BasicMotionSample
             this.check.Name = "check";
             this.check.Width = 20;
             // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.AutoSize = true;
+            this.groupBox1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.groupBox1.Controls.Add(this.buttonResetAll);
+            this.groupBox1.Controls.Add(this.buttonResetChecked);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Location = new System.Drawing.Point(3, 303);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Size = new System.Drawing.Size(305, 62);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // buttonResetAll
+            // 
+            this.buttonResetAll.Location = new System.Drawing.Point(201, 24);
+            this.buttonResetAll.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonResetAll.Name = "buttonResetAll";
+            this.buttonResetAll.Size = new System.Drawing.Size(100, 21);
+            this.buttonResetAll.TabIndex = 5;
+            this.buttonResetAll.Text = "Reset All";
+            this.buttonResetAll.UseVisualStyleBackColor = true;
+            this.buttonResetAll.Click += new System.EventHandler(this.buttonResetAll_Click);
+            // 
+            // buttonResetChecked
+            // 
+            this.buttonResetChecked.Location = new System.Drawing.Point(87, 24);
+            this.buttonResetChecked.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonResetChecked.Name = "buttonResetChecked";
+            this.buttonResetChecked.Size = new System.Drawing.Size(100, 21);
+            this.buttonResetChecked.TabIndex = 4;
+            this.buttonResetChecked.Text = "Reset Axes";
+            this.buttonResetChecked.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(18, 27);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(70, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "All check";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Location = new System.Drawing.Point(261, 369);
+            this.buttonClose.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(100, 21);
+            this.buttonClose.TabIndex = 6;
+            this.buttonClose.Text = "Close";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
             // FormAlarmMonitor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(349, 304);
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(372, 458);
+            this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.groupBox2);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "FormAlarmMonitor";
             this.Text = "Alarm Monitor";
+            this.Load += new System.EventHandler(this.FormAlarmMonitor_Load);
             this.groupBox2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -164,10 +253,16 @@ namespace BasicMotionSample
 
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.DataGridViewTextBoxColumn Axis;
         private System.Windows.Forms.DataGridViewTextBoxColumn Status;
         private System.Windows.Forms.DataGridViewButtonColumn Reset;
         private System.Windows.Forms.DataGridViewTextBoxColumn AlarmCode;
         private System.Windows.Forms.DataGridViewCheckBoxColumn check;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button buttonResetAll;
+        private System.Windows.Forms.Button buttonResetChecked;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button buttonClose;
     }
 }
--- MEVN SWM-G/SWM-G_BasicMotionSample/FormAlarmMonitor.cs
+++ MEVN SWM-G/SWM-G_BasicMotionSample/FormAlarmMonitor.cs
@@ -6,14 +6,109 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using SSCApiCLR;
 
 namespace BasicMotionSample
 {
     public partial class FormAlarmMonitor : Form
     {
+        private const int MaxAxiShow = 128;
+        private bool bEnableUpdate = true;
+
+        private SSCApiAxesMonitor sscApiAxesMonitor = null;     //SSCApiAxesMonitor class
+        private static int formCount = 0;                       //Number of display forms.
+        private SSCApiControl sscApiMotionCtrl = null;  //SSCApiControl class\
+        private CoreMotion sscApiCoreMotion = null;
         public FormAlarmMonitor()
         {
             InitializeComponent();
+        }
+
+        private void InitializeDataGridView()
+        {
+            /// dataGridView1 ////
+            dataGridView1.RowCount = MaxAxiShow;
+            for (int i = 0; i < MaxAxiShow; i++)
+            {
+                dataGridView1[0, i].Value = i < 10 ? "0" + i : i.ToString();//axis
+                dataGridView1[2, i].Value = "Rst";//Reset
+            }
+           //UpdateDataGridView();
+        }
+
+        private void UpdateDataGridView()
+        {
+            /// dataGridView1 ////
+            if (sscApiAxesMonitor.UpdateAxesStatus())
+            {
+                CoreMotionAxisStatus[] axisStatus = sscApiAxesMonitor.GetAxesStatus();
+                for (int i = 0 ; i < MaxAxiShow ; i++)
+                {
+                   UpdateAxesMonitor(i, ref axisStatus[i]);
+                    
+                }
+            }
+        }
+
+        private void FormAlarmMonitor_Load(object sender, EventArgs e)
+        {
+            if (CreateSSCApiObject())
+            {
+                if (sscApiAxesMonitor.CreateDevice() && sscApiMotionCtrl.CreateDevice())
+                {
+                    //-------------------------------------------------
+                    //Initial controls.
+                    timer1.Enabled = true;
+                    InitializeDataGridView();
+
+                }
+                else
+                {
+                    MessageBox.Show("Err");
+                    Close();
+                }
+            }
+        }
+
+        private bool CreateSSCApiObject()
+        {
+            sscApiAxesMonitor = new SSCApiAxesMonitor();
+            sscApiMotionCtrl = new SSCApiControl();
+            return ((sscApiAxesMonitor != null) && (sscApiMotionCtrl != null));
+        }
+
+        private void UpdateAxesMonitor(int row, ref CoreMotionAxisStatus axisStatus)
+        {
+            dataGridView1[1, row].Style.BackColor = axisStatus.ServoOn ? (axisStatus.HomeDone ? Color.Green : Color.Yellow) : (axisStatus.AmpAlarm ? Color.Red : Color.White);
+            dataGridView1.Rows[row].ReadOnly = axisStatus.ServoOffline ? true : false;
+            dataGridView1.Rows[row].DefaultCellStyle.BackColor = axisStatus.ServoOffline ? Color.DarkGray : Color.White;
+            dataGridView1[3, row].Value = axisStatus.AmpAlarm ? axisStatus.AmpAlarmCode.ToString() : "-";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            UpdateDataGridView();
+            timer1.Enabled = true;
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void buttonResetAll_Click(object sender, EventArgs e)
+        {
+            CoreMotionAxisStatus[] axisStatus = sscApiAxesMonitor.GetAxesStatus();
+            for (int i = 0; i < MaxAxiShow; i++)
+            {
+                int bRet = axisStatus[i].AmpAlarm ? sscApiCoreMotion.AxisControl.ClearAmpAlarm(i) : 0; 
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            
         }
     }
 }
